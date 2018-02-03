92e46c74d7ccb3d6f029838b8d98a24729b10a1c
Stéphane Travostino <steph@combo.cc>
2018-02-03 23:56:53 +0000

    Basic damage calculator

--- src/App.js
+++ src/App.js
@@ -1,8 +1,91 @@
 import React, { Component } from "react";
+import { calc_damage } from "./lib/damage";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      str: 11,
+      weapon_skill: 14,
+      fighting_skill: 14,
+      base_damage: 11,
+      enchantment: 5,
+      slay: 0
+    };
+  }
+
+  setField(fieldName) {
+    return event => {
+      this.setState({ [fieldName]: parseFloat(event.target.value) });
+    };
+  }
+
   render() {
-    return <div className="App">crawl stats</div>;
+    let damage = calc_damage(this.state);
+
+    return (
+      <div className="App">
+        <section>
+          <h2>Your stats</h2>
+          <label htmlFor="str">STR</label>
+          <input
+            type="number"
+            id="str"
+            value={this.state.str}
+            onChange={this.setField("str")}
+          />
+
+          <label htmlFor="weapon_skill">Weapon Skill</label>
+          <input
+            type="number"
+            id="weapon_skill"
+            value={this.state.weapon_skill}
+            onChange={this.setField("weapon_skill")}
+          />
+
+          <label htmlFor="fighting_skill">Fighting Skill</label>
+          <input
+            type="number"
+            id="fighting_skill"
+            value={this.state.fighting_skill}
+            onChange={this.setField("fighting_skill")}
+          />
+        </section>
+        <section>
+          <h2>Weapon stats</h2>
+
+          <label htmlFor="base_damage">Base Damage</label>
+          <input
+            type="number"
+            id="base_damage"
+            value={this.state.base_damage}
+            onChange={this.setField("base_damage")}
+          />
+
+          <label htmlFor="enchant">Enchantment</label>
+          <input
+            type="number"
+            id="enchant"
+            value={this.state.enchantment}
+            onChange={this.setField("enchantment")}
+          />
+
+          <label htmlFor="slay">Additional Slaying bonus</label>
+          <input
+            type="number"
+            id="slay"
+            value={this.state.slay}
+            onChange={this.setField("slay")}
+          />
+        </section>
+        <section>
+          <h2>Damage stats</h2>
+
+          <p>Average damage: {damage.avg()}</p>
+          <p>Max damage: {damage.max()}</p>
+        </section>
+      </div>
+    );
   }
 }
 
---
+++ src/lib/damage.js
@@ -0,0 +1,67 @@
+import { dice, dice0, constant } from "./expr";
+
+function base_damage(weapon_damage) {
+  return constant(weapon_damage);
+}
+
+function apply_stat_modifier(damage, str) {
+  let mod = constant(39);
+
+  if (str > 10) {
+    mod = mod.add(dice0(str - 9).mul(2));
+  } else if (str < 10) {
+    mod = mod.sub(dice0(11 - str).mul(3));
+  }
+
+  return damage.mul(mod).div(39);
+}
+
+function apply_weapon_skill(damage, weapon_skill) {
+  let mod = dice(weapon_skill * 100).add(2500);
+
+  return damage.mul(mod).div(2500);
+}
+
+function apply_fighting_skill(damage, fighting_skill) {
+  const base = 3000;
+
+  let mod = dice(fighting_skill * 100).add(base);
+
+  return damage.mul(mod).div(base);
+}
+
+function apply_misc_modifiers(damage) {
+  return damage;
+}
+
+// https://github.com/crawl/crawl/blob/7bf63f6c59ec0b09b03f62e0b917198a2f13f101/crawl-ref/source/attack.cc#L1183
+function apply_slaying_bonuses(damage, slaying_bonus) {
+  return damage.add(dice0(slaying_bonus));
+}
+
+// https://github.com/crawl/crawl/blob/7bf63f6c59ec0b09b03f62e0b917198a2f13f101/crawl-ref/source/attack.cc#L1442
+// function apply_brand(damage) {
+//   return damage;
+// }
+
+export function calc_damage({
+  str,
+  weapon_skill,
+  base_damage,
+  fighting_skill,
+  enchantment,
+  slay
+}) {
+  console.log(arguments);
+
+  let damage = dice(apply_stat_modifier(constant(base_damage), str).add(1));
+  damage = apply_weapon_skill(damage, weapon_skill);
+  damage = apply_fighting_skill(damage, fighting_skill);
+  damage = apply_misc_modifiers(damage);
+  damage = apply_slaying_bonuses(damage, slay + enchantment);
+  // TODO https://github.com/crawl/crawl/blob/db02301618d730d66a36defae39f69395b5df7a6/crawl-ref/source/melee-attack.cc#L1493
+  // TODO https://github.com/crawl/crawl/blob/db02301618d730d66a36defae39f69395b5df7a6/crawl-ref/source/melee-attack.cc#L1477
+  // damage = apply_brand(damage);
+
+  return damage;
+}
